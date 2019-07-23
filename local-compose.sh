f2686c2b72c2a439013deb4b80108d2a7b81bb4e
James Perkins <jperkins@redhat.com>
2019-07-22 17:46:05 -0700

    Allow WildFly Core custom built distributions to be used

--- local-compose.sh
+++ local-compose.sh
@@ -16,6 +16,7 @@ printArgHelp() {
 printHelp() {
     printArgHelp "-h" "Prints this help."
     printArgHelp "-p" "The path the to servers tar.gz file."
+    printArgHelp "-r" "Run the built docker immages with docker-compose up."
     printArgHelp "-v" "The version of WildFly to use."
     echo "Usage: ${0##*/} -v 17.0.1.Final"
 }
@@ -59,16 +60,24 @@ if [ -z ${wildfly_path} ]; then
     distFilename="wildfly-dist-${wildfly_version}.tar.gz"
     wildfly_path="${HOME}/.m2/repository/org/wildfly/wildfly-dist/${wildfly_version}/${distFilename}"
 else
-    distFilename="wildfly-${wildfly_version}.tar.gz"
-    wildfly_path="${wildfly_path}/dist/target/${distFilename}"
+    # Attempt to find the distribution
+    for f in $(find "${wildfly_path}" -name "*-${wildfly_version}.tar.gz"); do
+        echo "Found distribution ${f}"
+        if [ -z ${distFilename} ]; then
+            distFilename=$(basename ${f})
+            wildfly_path="${f}"
+        else
+            echo "Multiple distributations found, using ${f}"
+        fi
+    done
 fi
 
 if [ ! -f ${wildfly_path} ]; then
     fail "File ${wildfly_path} does not exist. Make sure you ran mvn clean install -pl dist -Pjboss-release -Prelease -DskipTests -Dcheckstyle.skip=true -Denforcer.skip=true"
 fi
 
-echo "wildfly_version=$wildfly_version"
-echo "wildfly_path=$wildfly_path"
+echo "wildfly_version=${wildfly_version}"
+echo "wildfly_path=${wildfly_path}"
 cp ${wildfly_path} ./wildfly/
 
 docker-compose -f docker-compose.yml -f docker-compose-local.yml build --build-arg WILDFLY_PATH=${distFilename} --build-arg WILDFLY_VERSION=${wildfly_version}
